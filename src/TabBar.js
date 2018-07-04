d8da5b8a1097487a54e18c679ff33578e63c08f7
aya <ayaahmed789@gmail.com>
2018-07-05 00:44:38 +0200

    keep selected route with same opacity

--- src/TabBar.js
+++ src/TabBar.js
@@ -405,7 +405,7 @@ export default class TabBar<T: *> extends React.Component<Props<T>, State> {
           >
             {routes.map((route, i) => {
               const outputRange = inputRange.map(
-                inputIndex => (inputIndex === i ? 1 : 0.7)
+                inputIndex => (inputIndex === i ? 1 : 1)
               );
               const opacity = Animated.multiply(
                 this.state.visibility,
